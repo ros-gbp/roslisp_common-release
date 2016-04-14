39a34123f7f9ae4b00c01696b965c8188328d1fd
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2016-04-14 11:30:04 +0200

    Rebase from 'debian/indigo/cl_tf2'

--- src/buffer-client.lisp
+++ src/buffer-client.lisp
@@ -28,76 +28,94 @@
 
 (in-package :cl-tf2)
 
+(defparameter *buffer-server-action-topic* "tf2_buffer_server")
+(defparameter *buffer-server-action-type* "tf2_msgs/LookupTransformAction")
+
 (defclass buffer-client ()
-  ((client :initarg :client 
-           :initform (actionlib:make-action-client
-                      "/tf2_buffer_server"
-                      "tf2_msgs/LookupTransformAction") 
-           :reader client)))
+  ((client :initarg :client
+           :initform (make-simple-action-client
+                      *buffer-server-action-topic*
+                      *buffer-server-action-type*)
+           :reader client)
+   (lock :initform (sb-thread:make-mutex :name (string (gensym "TF2-LOCK-")))
+         :accessor lock :type mutex)))
 
-(defmethod can-transform ((tf buffer-client) target-frame source-frame 
-                          &optional (source-time 0.0) (timeout 0.0)
-                            (target-time source-time target-time-supplied-p)
-                            (fixed-frame source-frame fixed-frame-supplied-p))
-  (ensure-both-or-none-supplied target-time-supplied-p fixed-frame-supplied-p
-                                target-frame source-frame)
-  (handler-case (if (and target-time-supplied-p fixed-frame-supplied-p)
-                    (lookup-Transform tf target-frame source-frame source-time timeout
-                                      target-time fixed-frame)
-                    (lookup-Transform tf target-frame source-frame source-time timeout))
-    (tf2-server-error () nil)))
-
-(defmethod lookup-transform ((tf buffer-client) target-frame source-frame 
-                             &optional (source-time 0.0) (timeout 0.0) 
-                               (target-time source-time target-time-supplied-p)
-                               (fixed-frame source-frame fixed-frame-supplied-p))
-  (ensure-both-or-none-supplied target-time-supplied-p fixed-frame-supplied-p
-                                source-frame target-frame)
-  (multiple-value-bind (result status)
-      (actionlib:send-goal-and-wait 
-       (client tf)
-       (actionlib:make-action-goal (client tf)
-         :target_frame target-frame :source_frame source-frame
-         :source_time source-time :timeout timeout
-         :target_time target-time :fixed_frame fixed-frame
-         :advanced (and target-time-supplied-p fixed-frame-supplied-p))
-       :result-timeout timeout)
-    (when (not (eq status :succeeded))
-      (error 'tf2-timeout-error :description "Action call did not succeed."))
-    (process-result result)))
-
-(defun ensure-both-or-none-supplied (target-time-supplied-p fixed-frame-supplied-p
-                                     source-frame target-frame)
-  (when (and target-time-supplied-p (not fixed-frame-supplied-p))
-    (error 'fixed-frame-missing-error
-           :source-frame source-frame
-           :target-frame target-frame)))
+(defmethod initialize-instance :after ((client buffer-client) &key (timeout 0.0))
+  (unless (wait-for-server (client client) timeout)
+    (error 'timeout-error :description "Waiting for action server timed out.")))
+  
+(defmethod lookup-transform ((tf buffer-client) target-frame source-frame
+                             &key time timeout target-time fixed-frame)
+  (declare (type string target-frame source-frame)
+           (type (or number null) time timeout target-time)
+           (type (or string null) fixed-frame))
+  ;; extra NIL checks are here in case the passed argument is explicitly set to NIL
+  (let ((source-time (or time 0.0))
+        (timeout (or timeout 0.0))
+        (target-time (or target-time 0.0))
+        (target-frame (unslash-frame target-frame))
+        (source-frame (unslash-frame source-frame))
+        (fixed-frame (unslash-frame fixed-frame)))
+    (sb-thread:with-recursive-lock ((lock tf))
+      (let* ((action-client (client tf))
+             (goal-msg (make-action-goal-msg action-client
+                         :target_frame target-frame
+                         :source_frame source-frame
+                         :source_time source-time
+                         :timeout timeout
+                         :target_time target-time
+                         :fixed_frame fixed-frame
+                         :advanced fixed-frame)) ; <- FIXED-FRAME is converted to a boolean here
+             (status
+               (send-goal-and-wait action-client goal-msg timeout timeout))
+             (result (result action-client)))
+        (unless (eq status :succeeded)
+          (error 'transform-stamped-error :description
+                 (if result
+                     (roslisp:msg-slot-value (roslisp:msg-slot-value result :error) :error_string)
+                     (format nil "Action call did not succeed. Status: ~a" status))))
+        (process-result result)))))
 
 (defun process-result (result)
   (with-fields (error transform) result
     (with-fields (error error_string) error
-      (cond ((eq error 
-                 (roslisp-msg-protocol:symbol-code
-                  'tf2_msgs-msg:tf2error :lookup_error))
-             (error 'tf2-lookup-error () :description error_string))
-            ((eq error 
-                 (roslisp-msg-protocol:symbol-code
-                  'tf2_msgs-msg:tf2error :connectivity_error))
-             (error 'tf2-connectivity-error () :description error_string))
-            ((eq error 
-                 (roslisp-msg-protocol:symbol-code
-                  'tf2_msgs-msg:tf2error :extrapolation_error))
-             (error 'tf2-extrapolation-error () :description error_string))
-            ((eq error 
-                 (roslisp-msg-protocol:symbol-code
-                  'tf2_msgs-msg:tf2error :invalid_argument_error))
-             (error 'tf2-invalid-argument-error () :description error_string))
-            ((eq error 
-                 (roslisp-msg-protocol:symbol-code
-                  'tf2_msgs-msg:tf2error :timeout_error))
-             (error 'tf2-timeout-error () :description error_string))
-            ((eq error 
-                 (roslisp-msg-protocol:symbol-code
-                  'tf2_msgs-msg:tf2error :transform_error))
-             (error 'tf2-transform-error () :description error_string))
-            (t (from-msg transform))))))
+      (ecase (roslisp-msg-protocol:code-symbol 'tf2_msgs-msg:tf2error error)
+        (:no_error (from-msg transform))
+        (:lookup_error (error 'lookup-error :description error_string))
+        (:connectivity_error (error 'connectivity-error :description error_string))
+        (:extrapolation_error (error 'extrapolation-error :description error_string))
+        (:invalid_argument_error (error 'invalid-argument-error :description error_string))
+        (:timeout_error (error 'timeout-error :description error_string))
+        (:transform_error (error 'transform-stamped-error :description error_string))))))
+
+(defmethod transform-pose-stamped ((tf buffer-client)
+                                   &key target-frame pose timeout use-current-ros-time)
+  (check-type target-frame string)
+  (check-type pose pose-stamped)
+  (let ((target-frame (unslash-frame target-frame))
+        (time (if use-current-ros-time
+                  (roslisp:ros-time)
+                  (or (stamp pose) 0.0)))
+        (source-frame (frame-id pose)))
+    (let ((transform (lookup-transform tf target-frame source-frame
+                                       :time time :timeout timeout)))
+      (pose->pose-stamped
+       target-frame
+       (stamp transform)
+       (cl-transforms:transform-pose transform pose)))))
+
+(defmethod transform-point-stamped ((tf buffer-client)
+                                    &key target-frame point timeout use-current-ros-time)
+  (check-type target-frame string)
+  (check-type point point-stamped)
+  (let ((target-frame (unslash-frame target-frame))
+        (time (if use-current-ros-time
+                  (roslisp:ros-time)
+                  (or (stamp point) 0.0)))
+        (source-frame (frame-id point)))
+    (let ((transform (lookup-transform tf target-frame source-frame
+                                       :time time :timeout timeout)))
+      (point->point-stamped
+       target-frame
+       (stamp transform)
+       (cl-transforms:transform-point transform point)))))
